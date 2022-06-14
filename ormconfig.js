2841fe53802416f556be128531ca2c2dc9c6c6eb
devfel <devfelizardo@gmail.com>
2022-06-14 10:54:22 -0400

    adding ssl config

--- ormconfig.js
+++ ormconfig.js
@@ -2,6 +2,7 @@ if (process.env.DATABASE_URL === "postgres://postgres:docker@localhost:5432/rper
     module.exports = {
         "type": "postgres",
         "url": process.env.DATABASE_URL,
+        "ssl": true, "extra": { "ssl": { "rejectUnauthorized": false } },
         "entities": [
             "./src/models/*.ts"
         ],
@@ -16,6 +17,7 @@ if (process.env.DATABASE_URL === "postgres://postgres:docker@localhost:5432/rper
     module.exports = {
         "type": "postgres",
         "url": process.env.DATABASE_URL,
+        "ssl": true, "extra": { "ssl": { "rejectUnauthorized": false } },
         "entities": [
             "./dist/models/*.ts"
         ],
