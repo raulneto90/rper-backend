b454f8bbf94f85faa6abc8e9ec6ad10861ba8be8
devfel <devfelizardo@gmail.com>
2022-08-03 20:31:59 -0400

    creating provider for storage options, innitialy just disk storage but later with S3 or Google Cloud

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -4,6 +4,7 @@ import uploadConfig from '@config/upload'
 import fs from 'fs'
 import { injectable, inject } from 'tsyringe';
 import IUsersRepository from '../repositories/IUsersRepository';
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 
 import AppError from '@shared/errors/AppError';
 
@@ -16,7 +17,11 @@ interface IRequest {
 class UpdateUserAvatarService {
     constructor(
         @inject('UsersRepository')
-        private usersRepository: IUsersRepository,) { }
+        private usersRepository: IUsersRepository,
+
+        @inject('StorageProvider')
+        private storageProvider: IStorageProvider,
+    ) { }
 
     public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
         const user = await this.usersRepository.findById(user_id);
@@ -27,15 +32,13 @@ class UpdateUserAvatarService {
 
         if (user.avatar) {
             //Delete previous avatar
-            const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-            const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
-
-            if (userAvatarFileExists) {
-                await fs.promises.unlink(userAvatarFilePath);
-            }
+            await this.storageProvider.deleteFile(user.avatar);
         }
 
-        user.avatar = avatarFilename;
+        const filename = await this.storageProvider.saveFile(avatarFilename);
+
+        user.avatar = filename;
+
         await this.usersRepository.save(user);
 
         return user;
