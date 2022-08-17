919d9c7e58c97856bdcdffe67b855853faa28319
devfel <devfelizardo@gmail.com>
2022-08-17 15:03:04 -0400

    creating userTokensRepository

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import rpersRouter from "@modules/rpers/infra/http/routes/rpers.routes";
 import usersRouter from "@modules/users/infra/http/routes/users.routes";
 import sessionsRouter from "@modules/users/infra/http/routes/sessions.routes";
+import passwordRouter from "@modules/users/infra/http/routes/password.routes";
 
 const routes = Router();
 
 routes.use("/users", usersRouter)
 routes.use("/rpers", rpersRouter)
 routes.use("/sessions", sessionsRouter)
+routes.use("/password", passwordRouter)
 
 export default routes;
