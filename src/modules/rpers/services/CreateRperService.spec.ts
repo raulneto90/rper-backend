3004ca81e024918d01fafbee9670403b322b19f4
devfel <devfelizardo@gmail.com>
2022-09-12 13:46:43 -0400

    refactoring tests to be more legible

--- src/modules/rpers/services/CreateRperService.spec.ts
+++ src/modules/rpers/services/CreateRperService.spec.ts
@@ -2,12 +2,18 @@ import FakeRpersRepository from '../repositories/fakes/FakeRpersRepository'
 import CreateRperService from './CreateRperService';
 import AppError from '@shared/errors/AppError';
 
+let fakeRpersRepository: FakeRpersRepository;
+let createRper: CreateRperService;
 
 describe('CreateRper', () => {
-    it('should be able to create a new RPER', async () => {
-        const fakeRpersRepository = new FakeRpersRepository();
-        const createRper = new CreateRperService(fakeRpersRepository);
+    beforeEach(() => {
+        fakeRpersRepository = new FakeRpersRepository();
+        createRper = new CreateRperService(
+            fakeRpersRepository,
+        );
+    });
 
+    it('should be able to create a new RPER', async () => {
         const rper = await createRper.execute({ name: "rper name example one", coordinator_id: "123456" });
 
         expect(rper).toHaveProperty('id');
@@ -16,9 +22,6 @@ describe('CreateRper', () => {
     });
 
     it('should not allow two RPERs with the same name', async () => {
-        const fakeRpersRepository = new FakeRpersRepository();
-        const createRper = new CreateRperService(fakeRpersRepository);
-
         await createRper.execute({ name: "rper name example one", coordinator_id: "123456" });
 
         expect(createRper.execute({ name: "rper name example one", coordinator_id: "789012" })).rejects.toBeInstanceOf(AppError);
--- src/modules/users/services/UpdateUserAvatarService.spec.ts
+++ src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -5,13 +5,18 @@ import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import UpdateUserAvatarService from './UpdateUserAvatarService';
 
+let fakeUsersRepository: FakeUsersRepository;
+let fakeStorageProvider: FakeStorageProvider;
+let updateUserAvatar: UpdateUserAvatarService;
+
 describe('UpdateUserAvatar', () => {
+    beforeEach(() => {
+        fakeUsersRepository = new FakeUsersRepository();
+        fakeStorageProvider = new FakeStorageProvider();
+        updateUserAvatar = new UpdateUserAvatarService(fakeUsersRepository, fakeStorageProvider);
+    });
+
     it('should be able to update avatar from user without any avatar yet', async () => {
-        const fakeUsersRepository = new FakeUsersRepository();
-        const fakeStorageProvider = new FakeStorageProvider();
-
-        const updateUserAvatar = new UpdateUserAvatarService(fakeUsersRepository, fakeStorageProvider);
-
         const user = await fakeUsersRepository.create({
             name: 'John Doe',
             email: 'johndoe@example.com',
@@ -27,12 +32,7 @@ describe('UpdateUserAvatar', () => {
     });
 
     it('should should not be able to update avatar from non existing user', async () => {
-        const fakeUsersRepository = new FakeUsersRepository();
-        const fakeStorageProvider = new FakeStorageProvider();
-
-        const updateUserAvatar = new UpdateUserAvatarService(fakeUsersRepository, fakeStorageProvider);
-
-        expect(
+        await expect(
             updateUserAvatar.execute({
                 user_id: 'non-existing-user',
                 avatarFilename: 'mock.jpg',
@@ -41,12 +41,7 @@ describe('UpdateUserAvatar', () => {
     });
 
     it('should be delete old avatar when updating to a new one', async () => {
-        const fakeUsersRepository = new FakeUsersRepository();
-        const fakeStorageProvider = new FakeStorageProvider();
-
         const deleteFile = jest.spyOn(fakeStorageProvider, 'deleteFile');
-
-        const updateUserAvatar = new UpdateUserAvatarService(fakeUsersRepository, fakeStorageProvider);
 
         const user = await fakeUsersRepository.create({
             name: 'John Doe',
